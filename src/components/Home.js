7ae8b85df31f28bf9f1596b3680a793cddc0deac
josephlee01 <bluewings113@gmail.com>
2021-08-31 12:59:08 +0900

    #7

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,25 @@
 const Home = () => {
-  return <h2>Home</h2>;
+  const onClick = (e) => {
+    console.log(e.target);
+  };
+
+  const onClickAgain = (name, e) => {
+    console.log(`Hello ${name}`, e.target);
+  };
+
+  return (
+    <div className="home">
+      <h2>Home</h2>
+      <button onClick={onClick}>Click me</button>
+      <button
+        onClick={(e) => {
+          onClickAgain("Joseph", e);
+        }}
+      >
+        Click me Again
+      </button>
+    </div>
+  );
 };
 
 export default Home;
