2bff9a817f7ea1ca6c4f00d33689927a7eeaf9a4
josephlee01 <bluewings113@gmail.com>
2021-09-01 13:21:09 +0900

    #20

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs, title,  }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
@@ -6,7 +6,6 @@ const BlogList = ({ blogs, title, handleDelete }) => {
         <div className="blog-preview" key={b.id}>
           <h2>{b.title}</h2>
           <p>Posted by {b.author}</p>
-          <button onClick={() => handleDelete(b.id)}>Delete Post</button>
         </div>
       ))}
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,19 @@
-import { useEffect, useState } from "react";
+import useFetch from "../useFetch";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-
-  // 함수를 만들어서 BlogList에 Props로 전달
-  const handleDelete = (id) => {
-    // filter는 기존 Array를 변형하지 않고 새로운 Array를 생성함.
-    const newBlogs = blogs.filter((b) => b.id !== id);
-    setBlogs(newBlogs);
-  };
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setBlogs(data);
-      });
-  }, []);
-
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
       {blogs && (
         <div>
-          <BlogList
-            blogs={blogs}
-            title="All Blogs!"
-            handleDelete={handleDelete}
-          />
+          <BlogList blogs={blogs} title="All Blogs!" />
           <BlogList
             blogs={blogs.filter((b) => b.author === "Joseph")}
             title="Joseph's Blogs!"
